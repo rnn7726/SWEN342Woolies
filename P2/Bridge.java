ec846230365483dc5d1f958fb60c4fbc20230620
Sam Falconer <sam@samfalconer.com>
2015-09-16 13:46:20 -0400

    Widen lock in P2

--- P2/Bridge.java
+++ P2/Bridge.java
@@ -1,29 +1,24 @@
 public class Bridge{
-	Object troll = new Object();
 	boolean onBridge;
 	
 	public Bridge(){
 		onBridge = false;
 	}
-	public void enterBridge(){
+	public synchronized void enterBridge(){
 		if (!onBridge){
 			onBridge = true;
 		} else{
 			while (onBridge){
-				synchronized(troll){
-					try {
-						troll.wait();
-					} catch (Exception e){e.printStackTrace();}
-				}
+				try {
+					wait();
+				} catch (Exception e){e.printStackTrace();}
 			}
 		}
 		
 	}
-	public void leaveBridge(){
-		synchronized (troll){
-			onBridge = false;
-			troll.notify();
-		}
+	public synchronized void leaveBridge(){
+		onBridge = false;
+		notify();
 	}
 
 
